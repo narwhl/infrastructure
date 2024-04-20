61781c17769dff3b77d90f883fe657abc5555652
Kristopher <890207+0xinterface@users.noreply.github.com>
2024-04-20 13:37:45 +0800

    updated flatcar ova url

--- scaffold/images.tf
+++ scaffold/images.tf
@@ -1,7 +1,7 @@
 resource "vsphere_content_library_item" "flatcar" {
   name        = "flatcar"
   description = "Flatcar Linux OVF Template"
-  file_url    = "https://stable.release.flatcar-linux.net/amd64-usr/current/flatcar_production_vmware_ova.ova"
+  file_url    = local.distros.flatcar.ova.url
   library_id  = vsphere_content_library.local.id
 
   lifecycle {
