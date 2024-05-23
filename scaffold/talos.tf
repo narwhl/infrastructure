b684e949b69f59286946810ba5b1dfb0c49b2f3a
Kristopher Lam <root@sandb0x.io>
2024-05-23 15:36:31 +0800

    added talos resources

--- scaffold/talos.tf
+++ scaffold/talos.tf
@@ -1,3 +1,19 @@
+locals {
+  cluster_name = data.external.env.result.GITHUB_REPOSITORY_OWNER
+  controlplanes = {
+    for controlplane in range(var.cluster.controlplane_count) : "k8s-c${controlplane}" => {
+      role = "controlplane"
+      idx  = controlplane
+    }
+  }
+  workers = {
+    for worker in range(var.cluster.worker_count) : "k8s-w${worker}" => {
+      role = "worker"
+      idx  = worker
+    }
+  }
+}
+
 data "http" "talos_customization" {
   url    = "https://factory.talos.dev/schematics"
   method = "POST"
@@ -19,3 +35,64 @@ data "http" "talos_customization" {
 }
 
 resource "talos_machine_secrets" "this" {}
+
+data "talos_machine_configuration" "machines" {
+  for_each         = merge(local.controlplanes, local.workers)
+  cluster_name     = local.cluster_name
+  cluster_endpoint = var.cluster_endpoint
+  machine_type     = each.value.role
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+  config_patches = [
+    yamlencode({
+      machine = {
+        install = {
+          extensions = [
+            {
+              image = "ghcr.io/siderolabs/qemu-guest-agent:8.2.2"
+            }
+          ]
+        }
+        network = {
+          interfaces = [
+            {
+              interface = [for interface in module.nodes[each.key].network_interface_names : interface if startswith(interface, "enx")][0]
+              addresses = [
+                format(
+                  "%s/%s",
+                  "${cidrhost("${var.network.block}/${var.network.suffix}", var.resource_alloc[each.value.role].address_start + each.value.idx)}",
+                  var.network.suffix
+                )
+              ]
+              routes = [
+                {
+                  network = "0.0.0.0/0"
+                  gateway = cidrhost("${var.network.block}/${var.network.suffix}", 1)
+                }
+              ]
+            }
+          ]
+        }
+      }
+      cluster = {
+        apiServer = {
+          extraArgs = {
+            "anonymous-auth" = true
+          }
+        }
+      }
+    })
+  ]
+}
+
+resource "talos_machine_configuration_apply" "nodes_config_apply" {
+  for_each                    = merge(local.controlplanes, local.workers)
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.machines[each.key].machine_configuration
+  node                        = module.nodes[each.key].ip_address
+}
+
+resource "talos_machine_bootstrap" "this" {
+  depends_on           = [talos_machine_configuration_apply.nodes_config_apply]
+  node                 = cidrhost("${var.network.block}/${var.network.suffix}", var.resource_alloc.controlplane.address_start)
+  client_configuration = talos_machine_secrets.this.client_configuration
+}
--- scaffold/variables.tf
+++ scaffold/variables.tf
@@ -3,3 +3,58 @@ variable "node_name" {
   default     = "pve"
   description = "Name of the Proxmox Host"
 }
+
+variable "storage" {
+  type    = string
+  default = "vmpool"
+}
+
+variable "cluster_endpoint" {
+  type = string
+}
+
+variable "network" {
+  type = object({
+    block  = string
+    suffix = number
+  })
+  default = {
+    block  = "172.27.1.0"
+    suffix = 24
+  }
+}
+
+variable "cluster" {
+  type = object({
+    controlplane_count = number
+    worker_count       = number
+  })
+  default = {
+    controlplane_count = 3
+    worker_count       = 3
+  }
+}
+
+
+variable "resource_alloc" {
+  type = map(object({
+    memory        = number
+    vcpus         = number
+    disk_size     = number
+    address_start = number
+  }))
+  default = {
+    "controlplane" = {
+      memory        = 4096
+      vcpus         = 4
+      disk_size     = 48
+      address_start = 30
+    }
+    "worker" = {
+      memory        = 16384
+      vcpus         = 8
+      disk_size     = 64
+      address_start = 40
+    }
+  }
+}
