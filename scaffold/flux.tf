ab6448194ad997cd2932d629a0db5b2d0f715b41
Kristopher Lam <root@sandb0x.io>
2024-05-23 16:15:18 +0800

    deploy flux

--- scaffold/flux.tf
+++ scaffold/flux.tf
@@ -1,3 +1,20 @@
+provider "flux" {
+  kubernetes = {
+    host = var.cluster_endpoint
+
+    client_certificate     = terraform_data.kubernetes_credentials.output.client_certificate
+    client_key             = terraform_data.kubernetes_credentials.output.client_key
+    cluster_ca_certificate = terraform_data.kubernetes_credentials.output.cluster_ca_certificate
+  }
+  git = {
+    url = "ssh://git@github.com/${data.external.env.result.GITHUB_REPOSITORY}.git"
+    ssh = {
+      username    = "git"
+      private_key = tls_private_key.flux.private_key_pem
+    }
+  }
+}
+
 resource "tls_private_key" "flux" {
   algorithm   = "ECDSA"
   ecdsa_curve = "P256"
@@ -8,4 +25,13 @@ resource "github_repository_deploy_key" "this" {
   repository = split("/", data.external.env.result.GITHUB_REPOSITORY)[1]
   key        = tls_private_key.flux.public_key_openssh
   read_only  = "false"
+}
+
+resource "flux_bootstrap_git" "this" {
+  depends_on = [
+    github_repository_deploy_key.this
+  ]
+
+  embedded_manifests = true
+  path               = "services/flux"
 }
