30063c8ec19a8467263d1d7f6e21ff95a42671d8
Kristopher Lam <root@sandb0x.io>
2024-05-23 15:46:38 +0800

    fixes node name scope

--- scaffold/compute.tf
+++ scaffold/compute.tf
@@ -2,7 +2,7 @@ module "nodes" {
   for_each       = merge(local.controlplanes, local.workers)
   source         = "github.com/narwhl/blueprint//modules/proxmox"
   name           = each.key
-  node           = local.node_name
+  node           = var.node_name
   vcpus          = var.resource_alloc[each.value.role].vcpus
   memory         = var.resource_alloc[each.value.role].memory
   disk_size      = var.resource_alloc[each.value.role].disk_size
