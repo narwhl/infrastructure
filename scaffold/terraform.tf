e9d756a6cda18a24d3387626e2ae06f1e5345f27
Kristopher Lam <root@sandb0x.io>
2024-05-23 16:15:39 +0800

    deploy flux

--- scaffold/terraform.tf
+++ scaffold/terraform.tf
@@ -3,6 +3,10 @@ terraform {
     key = "states/infrastructure/scaffold/terraform.tfstate"
   }
   required_providers {
+    flux = {
+      source  = "fluxcd/flux"
+      version = "1.3.0"
+    }
     github = {
       source  = "integrations/github"
       version = "6.2.1"
